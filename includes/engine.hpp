9eaf0f4b9bafb5a6e3e244e490a934fa1ff5008b
v01d3d <37172070+lindorffs@users.noreply.github.com>
2024-05-01 21:52:45 -0700

    Added textures, fonts, rendering commands, and LUA

--- includes/engine.hpp
+++ includes/engine.hpp
@@ -4,20 +4,63 @@
 // end define gaurd
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+#include <LUA/lua.hpp>
 
 #define TARGET_FPS 60 // How many frames per second are *desired*.
 #define TARGET_TPS 30 // How many update ticks per second are *desired*.
 
+#define MAX_TEXTURES 1024
+#define MAX_FONTS 32
+
+
+class texture {
+public:
+	const char *id = NULL;
+	SDL_Texture *data = NULL;
+	
+	int width = 0;
+	int height = 0;
+	
+	~texture();
+	texture();
+	texture(const char *id, const char *file);
+	
+	int render_at(int x, int y, double scale_x, double scale_y, bool centered, double rotation);
+};
+
+class font {
+public:
+	const char *id = NULL;
+	TTF_Font *data = NULL;
+	
+	~font();
+	font();
+	font(const char *id, const char *file, int size);
+	
+	int render_at(const char *text, int x, int y, int r, int g, int b, bool centered, double rotation);
+};
+
 class engine {
 private:
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
+public:
 	Uint32 last_render = 0; // in ms
 	Uint32 last_update = 0; // in ms
-public:
+	
+	texture *Textures[MAX_TEXTURES];
+	font *Fonts[MAX_FONTS];
+	SDL_Window *window = NULL;
+	lua_State *lua_instance = NULL;
+	SDL_Renderer *renderer = NULL;
 	Uint32 current_tick = 0; // the time since the engine began in ms
 
-	bool initialized = false;
+	int screen_width = 1000;
+	int screen_height = 700;
+
+	bool initialized = true;
 	bool running = false;
 
 	int loop_function(); // main loop function. calls render and update;
@@ -32,6 +75,16 @@ public:
 	void stop(); // called to stop the main loop;
 
 	engine(); // kinda just exists;
+	
+	int load_texture(const char *id,const char *file_name);
+	int render_texture(const char *id, int x, int y, double scale_x, double scale_y, bool centered, double rotation);
+	
+	int load_font(const char *id, const char *file_name, int size);
+	int render_text(const char *id, const char *text, int x, int y, int r, int g, int b, bool centered, double rotation);
+	void render_line(int x1, int y1, int x2, int y2, int r, int g, int b, int a);
+	void render_rectangle(int x, int y, int w, int h, int r, int g, int b, int a, bool centered);
+	void render_rectangle_border(int x, int y, int w, int h, int r, int g, int b, int a, bool centered);
 };
 
+engine* gEngine = NULL;
 #endif
